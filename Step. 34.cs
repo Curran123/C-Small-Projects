492010353e6ae51be3520bd51bf654339ff8166d
Curran123 <pcomeara@comcast.net>
2019-03-04 11:55:00 -0800

    Adding local files to new remote repository

---
+++ Step. 34.cs
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.ReadLine();
+        Console.WriteLine("Remember, we're 'accounting' on you!");
+        Console.ReadLine();
+    }
+=======
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.ReadLine();
+        Console.WriteLine("Remember, we're 'accounting' on you!");
+        Console.ReadLine();
+    }
+>>>>>>> a7294818993d987a2155eec0bfe7c4a586e945ed
+}
