69cfd0b3ff3d16a50787b5818965cda66631ab12
Curran123 <pcomeara@comcast.net>
2019-03-18 11:19:00 -0700

    Step 66.

---
+++ Step66Exercise/Step66Exercise/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step66Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Put in a number");
+            string input = Console.ReadLine();
+            int Input = Convert.ToInt32(input);
+            int product = Input * 50;
+            Console.WriteLine(product);
+            Console.ReadLine();
+
+            Console.WriteLine("Put in another number");
+            string ainput = Console.ReadLine();
+            int aInput = Convert.ToInt32(input);
+            int total = aInput + 25;
+            Console.WriteLine(total);
+            Console.ReadLine();
+
+            Console.WriteLine("Put in yet another number");
+            string binput = Console.ReadLine();
+            int bInput = Convert.ToInt32(input);
+            Console.WriteLine(bInput / 12.5);
+            Console.ReadLine();
+
+            Console.WriteLine("Once more, put in a number");
+            string cinput = Console.ReadLine();
+            int cInput = Convert.ToInt32(input);
+            if (cInput > 50)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+            Console.ReadLine();
+
+            Console.WriteLine("'Knock Knock,' 'Who's There?' 'ANOTHER NUMBER PLEASE'");
+            string dinput = Console.ReadLine();
+            int dInput = Convert.ToInt32(input);
+            Console.WriteLine(dInput / 7);
+            Console.ReadLine();
+            Console.WriteLine(dInput % 7);
+            Console.ReadLine();
+        }
+
+    }
+
+}
