c207cd120b2213fe8e38438a5c5c7863603afd39
Curran123 <pcomeara@comcast.net>
2019-03-19 13:35:27 -0700

    Changes.

--- Step67Exercise/Step67Exercise/Program.cs
+++ Step67Exercise/Step67Exercise/Program.cs
@@ -19,6 +19,9 @@ namespace Step67Exercise
             Console.WriteLine("Hours worked per week");
             string ainput = Console.ReadLine();
             int aInput = Convert.ToInt32(ainput);
+            int product = aInput * Input; //Weekly Rate for Person 1
+            product = 52 * product; //Product is now equal to Weekly Rate * 52 
+            Console.WriteLine(product);
             Console.ReadLine();
 
             Console.WriteLine("Person 2");
@@ -28,21 +31,30 @@ namespace Step67Exercise
             Console.WriteLine("Hours worked per week");
             string cinput = Console.ReadLine();
             int cInput = Convert.ToInt32(cinput);
+            int product2 = cInput * bInput;
+            product2 = 52 * product2;
+            Console.WriteLine(product2);
             Console.ReadLine();
 
 
 
-            Console.WriteLine("Annual salary of Person 1");
-            Console.WriteLine(15 * 8765);
-            Console.WriteLine();   //$31,200
+            //Console.WriteLine("Annual salary of Person 1");
+            //Console.WriteLine();
+            //Console.WriteLine();   //$31,200
 
-            Console.WriteLine("Annual salary of Person 2");
-            Console.WriteLine(20 * 8765);
-            Console.WriteLine();   //$41,600
+            //Console.WriteLine("Annual salary of Person 2");
+            //Console.WriteLine();
+            //Console.WriteLine();   //$41,600
 
             Console.WriteLine("Does Person 1 make more money than Person 2?");
-            Console.ReadLine();
-            Console.WriteLine("False");
+            if (product > product2)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
             Console.ReadLine();
         }
     }
