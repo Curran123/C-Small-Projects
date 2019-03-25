ebd2762a2b6a3d39980d6a20ee2a1a30fd57b190
Curran123 <pcomeara@comcast.net>
2019-03-25 14:35:14 -0700

    First.

---
+++ Step97Exercise/Step97Exercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step97Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StringBuilder sb = new StringBuilder("MY", 10);
+            sb.Append(new char[] { 'N', 'A', 'M', 'E', 'I', 'S' });
+            Console.WriteLine("{0} chars: {1}", sb.Length, sb.ToString());
+
+
+
+
+            string firstName = "peter";
+            string middleName = "curran";
+            string lastName = "o'meara";
+
+            
+            
+
+
+
+            string lower = "peter" + "curran" + "o'meara";
+            Console.WriteLine(lower.ToUpper());
+            Console.ReadLine();
+            
+            
+
+        }
+    }
+}
