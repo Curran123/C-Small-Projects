1d645171455b7d8927fbf3a87b54c242021ee6c2
Curran123 <pcomeara@comcast.net>
2019-03-29 17:19:13 -0700

    Pushing for my own remote reference.

---
+++ Step104Video/Step104Video/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step104Video
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EXCEPTION HANDLING PART ONE & TWO
+
+
+            //Exceptions are when something goes wrong in your code. 
+            //From a UX perspective, you don't want the user to see something go wrong it the code.
+            //In this example, if you put in "Jesse", you will get "Input string was not in a correct format.
+            
+            //Console.WriteLine("Pick a number.");
+            //int numberOne = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Pick a second number. ");
+            //int numberTwo = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Dividing the two...");
+            //int numberThree = numberOne / numberTwo;
+            //Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+            //Console.ReadLine();
+
+            //We solve exception handling through a trycatch block
+
+            //It essentially says "Let's try this, but catch any exceptions"
+
+            try
+            {
+                Console.WriteLine("Pick a number.");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number. ");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+                Console.ReadLine();
+            }
+            catch (FormatException ex) //Only catching a format exception, and assigning it the variable "ex", which is a datatype format exception
+            {
+                Console.WriteLine(ex.Message); //Could write "Please type a whole number" to customize UX
+            }
+            
+
+            //different error
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally //Runs no matter what. However, does not catch errors which could be logged for other purposes.
+                    
+            {
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }   
+    }
+}
---
+++ Step106Exercise/Step106Exercise/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step106Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //STEP 1.
+            //Create a list of integers. Ask the user for a number to divide each number in the list by. 
+            //Write a loop that takes each integer in the list, divides it by the number the user entered, 
+            //and displays the result to the screen.
+
+
+            List<int> Numbers = new List<int>();
+            Numbers.Add(0);
+            Numbers.Add(1);
+            Numbers.Add(2);
+            Numbers.Add(3);
+            Numbers.Add(4);
+            Numbers.Add(5);
+            Numbers.Add(6);
+
+            //Console.WriteLine(0);
+            //Console.WriteLine(1);
+            //Console.WriteLine(2);
+            //Console.WriteLine(3);
+            //Console.WriteLine(4);
+            //Console.WriteLine(5);
+            //Console.WriteLine(6);
+            List<int> Numbers2 = new List<int>(); //Second List
+            Console.WriteLine("Please select a number to divide each other number by");
+            int count = 0;
+            String n = Console.ReadLine();
+            int z = Convert.ToInt32(n);
+            
+
+            foreach (int s in Numbers)
+
+            {
+
+                Numbers2.Add(s / z);
+                Console.WriteLine(s);
+
+            }
+            foreach (int s in Numbers2)
+
+            {
+
+               
+                Console.WriteLine(s);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
