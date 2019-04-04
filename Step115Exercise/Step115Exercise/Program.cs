3ddcb93ca5e2a4de1072992a0ac3fc7cbd7de535
Curran123 <pcomeara@comcast.net>
2019-04-03 19:49:24 -0700

    First

---
+++ Step115Exercise/Step115Exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step115Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoIntegers p = new TwoIntegers();
+            Console.WriteLine("Please give me a number");
+            int k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please give me a second number if you want");
+            string j = Console.ReadLine();
+            if (string.IsNullOrEmpty(j)) {
+                Console.WriteLine(p.FirstTwo(k));//will just call the function with just one parameter.          
+            }
+            else
+            {
+                int J = Convert.ToInt32(j);
+                Console.WriteLine(p.FirstTwo(k, J));
+            }
+
+          
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Step115Exercise/Step115Exercise/TwoIntegers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step115Exercise
+{
+    public class TwoIntegers
+    {
+        public int FirstTwo(int TheFirstOne, int num2 = 0)
+        {
+            
+
+            return TheFirstOne + num2;
+
+            
+        }
+
+         
+        
+        
+    }
+}
